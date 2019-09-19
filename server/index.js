4f4f0d644c45df31a50c428851da245ee6e41847
Dio Adhita Saputra <dio.adhita@gmail.com>
2019-09-20 01:15:06 +0700

    rm

--- server/index.js
+++ server/index.js
@@ -27,9 +27,9 @@ async function start () {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(heroku_port, heroku_host)
+  app.listen(port, host)
   consola.ready({
-    message: `Server listening on http://${heroku_host}:${heroku_port}`,
+    message: `Server listening on http://${host}:${port}`,
     badge: true
   })
 }
