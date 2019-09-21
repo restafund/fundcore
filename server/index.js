a1cd71571333a283c17bb968feb604fd0da79f80
Dio Adhita Saputra <dio.adhita@gmail.com>
2019-09-21 19:22:04 +0700

    Update server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
+// const { host, port } = nuxt.options.server
+const heroku_host = process.env.HOST || '127.0.0.1'
+const heroku_port = process.env.PORT || 5000
+
+app.set('port', heroku_port)
+
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
@@ -10,10 +16,6 @@ config.dev = process.env.NODE_ENV !== 'production'
 async function start () {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
-
-  // const { host, port } = nuxt.options.server
-  const heroku_host = process.env.HOST || host || '127.0.0.1'
-  const heroku_port = process.env.PORT || port || 5000
 
   // Build only in dev mode
   if (config.dev) {
